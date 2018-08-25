3b2098e1fc5c3b179bd7c933901b14832807d3e5
jeremydumais@hotmail.com <gIyqyc8PJNDVn0chDDqg>
2018-08-25 17:17:24 -0400

    Initial commit

---
+++ include/prime.h
@@ -0,0 +1,11 @@
+#ifndef PRIME_H
+#define PRIME_He
+
+class prime
+{
+    public:
+        static bool isPrime(unsigned int p_number);
+    private:
+};
+
+#endif //PRIME_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <conio.h>
+#include "include/prime.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
